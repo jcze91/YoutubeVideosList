68a54fbdbf8a6e825d391fe6a21cef62f0a38367
Oussama Ezzayer <oussama@ezzayer.net>
2014-12-19 03:29:10 +0100

    Added video list view (psuedo material design style cards) + misc. changes

--- app/src/main/java/ytvideoslist/mti/com/ytvideoslist/MainActivity.java
+++ app/src/main/java/ytvideoslist/mti/com/ytvideoslist/MainActivity.java
@@ -2,6 +2,7 @@ package ytvideoslist.mti.com.ytvideoslist;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
@@ -11,10 +12,17 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+
+import it.gmariotti.cardslib.library.cards.material.MaterialLargeImageCard;
+import it.gmariotti.cardslib.library.internal.Card;
+import it.gmariotti.cardslib.library.internal.CardArrayAdapter;
+import it.gmariotti.cardslib.library.view.CardListView;
+
 
 public class MainActivity extends ActionBarActivity {
 
-  // TODO Remove the TAG for final version
+  // TODO Remove possible useless debug logs prior to submission
   private static final String TAG = "MainActivity";
 
   @Override
@@ -23,23 +31,19 @@ public class MainActivity extends ActionBarActivity {
     setContentView(R.layout.activity_main);
     if (savedInstanceState == null) {
       getSupportFragmentManager().beginTransaction()
-          .add(R.id.container, new PlaceholderFragment())
+          .add(R.id.container, new VideoListFragment())
           .commit();
     }
   }
 
   @Override
   public boolean onCreateOptionsMenu(Menu menu) {
-    // Inflate the menu; this adds items to the action bar if it is present.
     getMenuInflater().inflate(R.menu.menu_main, menu);
     return true;
   }
 
   @Override
   public boolean onOptionsItemSelected(MenuItem item) {
-    // Handle action bar item clicks here. The action bar will
-    // automatically handle clicks on the Home/Up button, so long
-    // as you specify a parent activity in AndroidManifest.xml.
     int id = item.getItemId();
 
     switch (id) {
@@ -47,7 +51,6 @@ public class MainActivity extends ActionBarActivity {
         Log.d(TAG, "Sharing is caring");
         return true;
       case R.id.action_about:
-        Log.d(TAG, "About screen");
         navigateToAbout();
         return true;
       default:
@@ -63,18 +66,52 @@ public class MainActivity extends ActionBarActivity {
   }
 
   /**
-   * A placeholder fragment containing a simple view.
+   * The fragment containing the video list.
    */
-  public static class PlaceholderFragment extends Fragment {
+  public static class VideoListFragment extends Fragment {
 
-    public PlaceholderFragment() {
+    public VideoListFragment() {
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
       View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
       return rootView;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+      super.onActivityCreated(savedInstanceState);
+
+      ArrayList<Card> cards = new ArrayList<>();
+
+      // TODO Fetch data from the API and populate cards based on Video Model
+      for (int i = 0; i < 5; ++i) {
+        MaterialLargeImageCard card =
+            MaterialLargeImageCard.with(getActivity())
+                .setTextOverImage("")
+                .setTitle("Google unveils Google Glass Explorer Edition at I/O - CNET News")
+                .setSubTitle("CNETTV")
+                .useDrawableId(R.drawable.video_thumb)
+                .build();
+        card.setOnClickListener(new Card.OnCardClickListener() {
+          @Override
+          public void onClick(Card card, View view) {
+            // TODO Open detailed view for the current card
+          }
+        });
+
+        cards.add(card);
+      }
+
+      CardArrayAdapter mCardArrayAdapter = new CardArrayAdapter(getActivity(), cards);
+
+      CardListView listView = (CardListView) getActivity().findViewById(R.id.videoList);
+      if (listView != null) {
+        listView.setAdapter(mCardArrayAdapter);
+      }
     }
   }
 }
