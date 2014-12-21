12554ef3abad213535dc9cd7e73d77a3530945c6
Yayap <nicolas.sabella@epita.fr>
2014-12-21 18:03:11 +0100

    add viewPagerAdapter

---
+++ app/src/main/java/ytvideoslist/mti/com/ytvideoslist/utils/ViewPagerAdapter.java
@@ -0,0 +1,30 @@
+package ytvideoslist.mti.com.ytvideoslist.utils;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import ytvideoslist.mti.com.ytvideoslist.fragments.VideoDetailFragment;
+import ytvideoslist.mti.com.ytvideoslist.fragments.VideoListFragment;
+import ytvideoslist.mti.com.ytvideoslist.models.Video;
+
+/**
+ * Created by Yayap on 21/12/14.
+ */
+public class ViewPagerAdapter extends FragmentPagerAdapter {
+
+    public ViewPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        Video v = VideoListFragment.videoList.get(position);
+        return VideoDetailFragment.newInstance(v);
+    }
+
+    @Override
+    public int getCount() {
+        return VideoListFragment.videoList.size();
+    }
+}
